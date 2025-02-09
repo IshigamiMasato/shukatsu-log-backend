42c976accb7912656e367e206e12944ce86e28b2
M I <mi@emunoMacBook-Pro.local>
2025-02-09 15:53:42 +0900

    企業登録APIを追加

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\CompanyService;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    /** @var \App\Services\CompanyService */
+    private $service;
+
+    public function __construct(CompanyService $companyService)
+    {
+        $this->service = $companyService;
+    }
+
+    public function store(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $userId = $request->user_id;
+
+        $postedParams = $request->only([
+            'name',
+            'url',
+            'president',
+            'address',
+            'establish_date',
+            'employee_number',
+            'listing_class',
+            'benefit',
+            'memo'
+        ]);
+
+        $result = $this->service->validateStore($postedParams);
+        if ( isset($result['errors']) ) {
+            return response()->badRequest( errors: $result['errors'] );
+        }
+
+        return $this->service->store($userId, $postedParams);
+    }
+}
