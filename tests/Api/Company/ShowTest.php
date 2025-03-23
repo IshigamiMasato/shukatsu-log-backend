c59eaf044ec10aed1a535bc3470e99f5db52ef10
M I <mi@emunoMacBook-Pro.local>
2025-03-23 20:05:10 +0900

    CompanyController@showのテスト追加

---
+++ tests/Api/Company/ShowTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Api\Company;
+
+use App\Models\Company;
+use App\Models\User;
+use Carbon\Carbon;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\Api\TestCaseWithAuth;
+
+class ShowTest extends TestCaseWithAuth
+{
+    /** @var string */
+    private $method = 'GET';
+
+    /** @var string */
+    private $path = '/api/company/{company_id}';
+
+    public function test_get_company()
+    {
+        $company = Company::factory()->create(['user_id' => $this->user->user_id]);
+
+        $path = preg_replace('/{.*}/', $company->company_id, $this->path);
+
+        $this->json($this->method, $path, [], ['Authorization' => 'Bearer ' . $this->token]);
+
+        $this->response->assertStatus(Response::HTTP_OK);
+        $this->assertValidateResponse($this->method, $this->path, $this->response);
+    }
+
+    public function test_get_company_invalid_token()
+    {
+        $company = Company::factory()->create(['user_id' => $this->user->user_id]);
+
+        $path = preg_replace('/{.*}/', $company->company_id, $this->path);
+
+        $this->json($this->method, $path, [], ['Authorization' => 'Bearer ' . 'Invalid Token']);
+
+        $this->response->assertStatus(Response::HTTP_UNAUTHORIZED);
+        $this->assertEquals(config('api.response.code.unauthorized'), $this->response->json()['code']);
+        $this->assertValidateResponse($this->method, $this->path, $this->response);
+    }
+
+    public function test_get_company_expired_token()
+    {
+        $company = Company::factory()->create(['user_id' => $this->user->user_id]);
+
+        $path = preg_replace('/{.*}/', $company->company_id, $this->path);
+
+        Carbon::setTestNow(Carbon::now()->addHours(2));
+
+        $this->json($this->method, $path, [], ['Authorization' => 'Bearer ' . $this->token]);
+
+        Carbon::setTestNow();
+
+        $this->response->assertStatus(Response::HTTP_UNAUTHORIZED);
+        $this->assertEquals(config('api.response.code.expired_token'), $this->response->json()['code']);
+        $this->assertValidateResponse($this->method, $this->path, $this->response);
+    }
+
+    public function test_get_company_user_not_found()
+    {
+        $company = Company::factory()->create(['user_id' => $this->user->user_id]);
+
+        $path = preg_replace('/{.*}/', $company->company_id, $this->path);
+
+        User::where('email', $this->user->email)->delete();
+
+        $this->json($this->method, $path, [], ['Authorization' => 'Bearer ' . $this->token]);
+
+        $this->response->assertStatus(Response::HTTP_NOT_FOUND);
+        $this->assertEquals(config('api.response.code.user_not_found'), $this->response->json()['code']);
+        $this->assertValidateResponse($this->method, $this->path, $this->response);
+    }
+
+    public function test_get_company_company_not_found()
+    {
+        $notExistsCompanyId = 9999999;
+
+        $path = preg_replace('/{.*}/', $notExistsCompanyId, $this->path);
+
+        $this->json($this->method, $path, [], ['Authorization' => 'Bearer ' . $this->token]);
+
+        $this->response->assertStatus(Response::HTTP_NOT_FOUND);
+        $this->assertEquals(config('api.response.code.company_not_found'), $this->response->json()['code']);
+        $this->assertValidateResponse($this->method, $this->path, $this->response);
+    }
+}
