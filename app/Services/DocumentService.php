3f54247f8f0251418c1c67b5c61a55311c280be8
M I <mi@emunoMacBook-Pro.local>
2025-03-25 19:10:54 +0900

    DocumentController@storeのテスト追加

--- app/Services/DocumentService.php
+++ app/Services/DocumentService.php
@@ -116,17 +116,19 @@ class DocumentService extends Service
             $document = $this->documentRepository->create($documentParams);
 
             // ファイルをストレージへ保存 && DBにファイルアップロード情報を保存
-            foreach ( $postedParams['files'] as $file ) {
-                $fileName = $file['name'];
-                $filePath = $this->getFilePath($userId, Str::uuid() . '_' . $fileName);
-                $this->uploadFile($filePath, $file['base64']);
+            if ( isset($postedParams['files']) ) {
+                foreach ( $postedParams['files'] as $file ) {
+                    $fileName = $file['name'];
+                    $filePath = $this->getFilePath($userId, Str::uuid() . '_' . $fileName);
+                    $this->uploadFile($filePath, $file['base64']);
 
-                $fileParams = [
-                    'document_id' => $document->document_id,
-                    'name'        => $fileName,
-                    'path'        => $filePath,
-                ];
-                $this->fileRepository->create($fileParams);
+                    $fileParams = [
+                        'document_id' => $document->document_id,
+                        'name'        => $fileName,
+                        'path'        => $filePath,
+                    ];
+                    $this->fileRepository->create($fileParams);
+                }
             }
 
             DB::commit();
