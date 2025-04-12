aab89f39dc5cfe5c59514109bec92824dfcb5472
M I <mi@emunoMacBook-Pro.local>
2025-04-12 19:42:00 +0900

    ゲストユーザのデータを追加

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +14,11 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(5)->create();
+        User::factory()->create();
+
+        User::factory()->create([
+            'email'    => 'guest@example.com',
+            'password' => Hash::make('guestpass'),
+        ]);
     }
 }
